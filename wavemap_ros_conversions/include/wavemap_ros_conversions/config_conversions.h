4ad345e156fea07fe35e6ee3414e9d4b1f3f7112
elliotirving <elliotirving44@hotmail.com>
2025-03-18 15:23:08 +0100

    config_conversions.h halfway ported

---
+++ wavemap_ros_conversions/include/wavemap_ros_conversions/config_conversions.h
@@ -0,0 +1,20 @@
+#ifndef WAVEMAP_ROS_CONVERSIONS_CONFIG_CONVERSIONS_H_
+#define WAVEMAP_ROS_CONVERSIONS_CONFIG_CONVERSIONS_H_
+
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+#include <wavemap/core/config/config_base.h>
+// #include <xmlrpcpp/XmlRpcValue.h> #TODO load params for ROS2 properly (elliot.irving)
+
+namespace wavemap::param::convert {
+param::Map toParamMap(const rclcpp::Node::SharedPtr& node, const std::string& ns);
+param::Array toParamArray(const rclcpp::Node::SharedPtr& node, const std::string& ns);
+param::Value toParamValue(const rclcpp::Node::SharedPtr& node, const std::string& ns);
+
+// param::Map toParamMap(const XmlRpc::XmlRpcValue& xml_rpc_value);
+// param::Array toParamArray(const XmlRpc::XmlRpcValue& xml_rpc_value);
+// param::Value toParamValue(const XmlRpc::XmlRpcValue& xml_rpc_value);
+}  // namespace wavemap::param::convert
+
+#endif  // WAVEMAP_ROS_CONVERSIONS_CONFIG_CONVERSIONS_H_
