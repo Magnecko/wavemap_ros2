a4dcf0c5512af562229f9c2a33ed7f8f8eddbc87
elliotirving <elliotirving44@hotmail.com>
2025-03-25 02:40:30 +0100

    time_conversions.h builds

---
+++ wavemap_ros_conversions/include/wavemap_ros_conversions/time_conversions.h
@@ -0,0 +1,24 @@
+#ifndef WAVEMAP_ROS_CONVERSIONS_TIME_CONVERSIONS_H_
+#define WAVEMAP_ROS_CONVERSIONS_TIME_CONVERSIONS_H_
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace wavemap::convert {
+inline rclcpp::Time nanoSecondsToRosTime(uint64_t nsec) {
+  constexpr uint64_t kSecToNsec = 1000000000ull;
+  return rclcpp::Time(static_cast<int32_t>(nsec / kSecToNsec),
+                      static_cast<int32_t>(nsec % kSecToNsec));
+}
+
+inline double nanoSecondsToSeconds(uint64_t nsec) {
+  constexpr double kNsecToSec = 1e-9;
+  return static_cast<double>(nsec) * kNsecToSec;
+}
+
+inline uint64_t rosTimeToNanoSeconds(const rclcpp::Time& time) {
+  constexpr uint64_t kSecToNsec = 1000000000ull;
+  return static_cast<uint64_t>(time.seconds()) * kSecToNsec + time.nanoseconds();
+}
+}  // namespace wavemap::convert
+
+#endif  // WAVEMAP_ROS_CONVERSIONS_TIME_CONVERSIONS_H_
--- wavemap_ros_conversions/src/test_include.cpp
+++ wavemap_ros_conversions/src/test_include.cpp
@@ -1,3 +1,4 @@
 #include <wavemap_ros_conversions/config_conversions.h>
 #include <wavemap_ros_conversions/geometry_msg_conversions.h>
-#include <wavemap_ros_conversions/map_msg_conversions.h>    
+#include <wavemap_ros_conversions/map_msg_conversions.h>
+#include <wavemap_ros_conversions/time_conversions.h>
